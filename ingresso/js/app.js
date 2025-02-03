44b00eaa865a47ace7cb41bc078f7f926d842af4
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-02-03 18:09:13 -0300

    pegando alguns valores para teste

---
+++ ingresso/js/app.js
@@ -0,0 +1,11 @@
+function comprar(){
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidadeIngressos = document.getElementById('qtd').value;
+
+    let quantPista = document.getElementById('qtd-pista');
+    let quantSuperior = document.getElementById('qtd-superior');
+    let quantInferior = document.getElementById('qtd-inferior');
+
+    alert(quantPista)
+    alert(quantidadeIngressos)
+}
