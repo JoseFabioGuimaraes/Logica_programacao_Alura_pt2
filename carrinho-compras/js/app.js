41d7e6816e1cb887a784fa72a4d1bb5ea2a26bea
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-02-03 17:27:34 -0300

    Initialize total value and implement cart clearing functionality

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,3 +1,5 @@
+let valorTotalGeral;
+limpar();
 function adicionar(){
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
@@ -5,15 +7,23 @@ function adicionar(){
     let quantidade = document.getElementById('quantidade');
     let valorTotal = valorUnitario * quantidade.value;
 
+    if (quantidade.value == 0){
+      return;
+    }
+
     let carrinho = document.getElementById('lista-produtos')
     carrinho.innerHTML =  carrinho.innerHTML + `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade.value}x</span> ${nomeProduto} <span class="texto-azul">R$${valorTotal}</span>
         </section>`
 
-
-
+    valorTotalGeral = valorTotalGeral + valorTotal;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$${valorTotalGeral}`;
+    quantidade.value = 0;
 }
 
 function limpar(){
-
+  valorTotalGeral = 0;
+  document.getElementById('lista-produtos').innerHTML = '';
+  document.getElementById('valor-total').innerHTML = 'R$ 0';
 }
